c4425064b94e0e98e5bee451083f705cc2876e07
Boby3867 <bobyb3867@gmail.com>
2026-09-17 19:05:46 +0000

    updata

--- 000_Labs/010_random_method-static/base_code/starter.java
+++ 000_Labs/010_random_method-static/base_code/starter.java
@@ -9,9 +9,9 @@ import java.util.Random;
 class starter {
 	public static void main(String args[]) {
 		int x = (int) Math.random();
-		System.out.println((int) (Math.random() * 10));
-		System.out.println((int) (Math.random() * 99 + 1));
-		System.out.println(Math.random() + 2.5);
-		System.out.println(Math.random() * 575 + 14);
+		System.out.println("A number between 0 - 9: " + (int) (Math.random() * 10));
+		System.out.println("A number between 1 - 10: " + (int) (Math.random() * 99 + 1));
+		System.out.println("A number between 2.5 and 3.5: " + Math.random() + 2.5);
+		System.out.println("A double between 14 and 589: " + Math.random() * 575 + 14);
 	}
 }
